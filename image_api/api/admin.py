99fc296e46ce05b0862637fff2d4088a6f9d8508
p2vvel <p2vvel@gmail.com>
2022-07-10 18:27:30 +0200

    Create models

--- image_api/api/admin.py
+++ image_api/api/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Tier, User, UploadedImage, AvailableHeight
+
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+
+
+class UploadedImageAdmin(admin.ModelAdmin):
+    model = UploadedImage
+
+
+class AvailableHeightAdmin(admin.ModelAdmin):
+    model = AvailableHeight
+
+
+class TierAdmin(admin.ModelAdmin):
+    model = Tier
+    inlines = [AvailableHeight]
+
+
+admin.site.register(Tier)
+admin.site.register(User)
+admin.site.register(UploadedImage)
+admin.site.register(AvailableHeight)
--- image_api/api/models.py
+++ image_api/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class AvailableHeight(models.Model):
+    height = models.IntegerField()      # available height of the image
+    
+    def __str__(self) -> str:
+        return f"Height: {self.height}px"
+
+
+class Tier(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)        # tier name
+    binary_image = models.BooleanField(default=False, null=False)           # is binary image available?
+    original_image = models.BooleanField(default=False, null=False)         # is original image available?
+    available_heights = models.ManyToManyField(to=AvailableHeight)          # available image heights (different idea => ArrayField => worse portability(only Postgres))
+
+    def __str__(self) -> str:
+        return f"'{self.name}' tier"
+
+
+class User(AbstractUser):
+    tier = models.ForeignKey(to=Tier, default=None, null=True, blank=True, on_delete=models.SET_NULL)   # null = basic tier
+
+
+class UploadedImage(models.Model):
+    image = models.ImageField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)        # user that uploaded image
+
