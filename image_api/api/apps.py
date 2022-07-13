8b9eb21731e888c38cc2e26c27bbe839a3f45852
p2vvel <p2vvel@gmail.com>
2022-07-14 00:49:00 +0200

    Signals removed

--- image_api/api/apps.py
+++ image_api/api/apps.py
@@ -4,10 +4,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    # signals has to be "started" manually by importing the module
-    def ready(self):
-        """
-        Executes signals at start
-        """
-        import api.signals
